d80570d3876ed92bd72796d33f9eb9ac16c0e1f3
pereiramemo <pereiramemo@gmail.com>
2018-11-01 23:52:49 +0100

    update

--- scripts/clust2abund2taxa_processing.R
+++ scripts/clust2abund2taxa_processing.R
@@ -4,7 +4,8 @@ library(tidyverse)
 ### 1. Load table
 ###############################################################################
 
-ABUND_TBL_long <- read_tsv(file = "~/Google_Drive/Doctorado/workspace/lagunas_16S_analysis/tables/clust2abund2taxa.tsv.gz",
+ABUND_TBL_long <- read_tsv(file = "/bioinf/home/epereira/workspace/16S_analyses/\
+lagunas_16S_analysis2/taxa_annot/amp2compare/clust2abund2taxa.tsv.gz",
                            col_names = T)
 
 
@@ -14,13 +15,11 @@ ABUND_TBL_long <- read_tsv(file = "~/Google_Drive/Doctorado/workspace/lagunas_16
 
 # note: the column names are cluster, sample, abund, seq_rep and taxonomy
 
-
 ABUND_TBL_long_redu <- ABUND_TBL_long %>%
                        group_by(cluster) %>% # group table by cluster, this allows the application of functions within each cluster
-                       mutate(total_abund = sum(abund)) %>% # create a new variable total_sum, total_abund per cluster
+                       mutate(total_abund = sum(abund)) %>% # create a new variable with total abund per cluster
                        filter(total_abund > 1) %>% # select cluster with total_abund greater than x. In this case x = 1.
                        select(cluster, sample, abund , taxonomy) # select relevant columns
- 
                        
 ###############################################################################
 ### 3. Convert to wide format
