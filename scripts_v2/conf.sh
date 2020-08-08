3df14ee7682467926af1390a013780eee5c6c615
pereiramemo <pereiramemo@gmail.com>
2020-08-08 17:27:06 +0000

    name update

--- scripts_v2/conf.sh
+++
@@ -1,7 +0,0 @@
-# dits
-REPOS="/home/epereira/workspace/repositories/"
-AMPLICON_PIPELINE_SCRIPTSV2="${REPOS}/amplicon_pipelines/scripts_v2/"
-
-# tools
-dada2_pipeline="${AMPLICON_PIPELINE_SCRIPTSV2}/dada2_pipeline.R"
-primers_removal="${AMPLICON_PIPELINE_SCRIPTSV2}/primers_removal.R"
