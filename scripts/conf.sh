bfc7fedbc00480d6b3caba4570103afe91749cec
pereiramemo <pereiramemo@gmail.com>
2020-08-08 17:17:27 +0000

    added scripts folder

---
+++ scripts/conf.sh
@@ -0,0 +1,11 @@
+# dits
+REPOS="/home/epereira/workspace/repositories/"
+AMPLICON_PIPELINE_SCRIPTSV2="${REPOS}/amplicon_pipelines/scripts_v2/"
+RESOURCES="/home/bioinf/resources/"
+SILVA_DIR="${RESOURCES}/silva/silva_v138/"
+BLAST_DB_DIR="${RESOURCES}/silva/silva_v138/blastdb"
+
+# tools
+dada2_pipeline="${AMPLICON_PIPELINE_SCRIPTSV2}/dada2_pipeline.R"
+primers_removal="${AMPLICON_PIPELINE_SCRIPTSV2}/primers_removal.R"
+taxa_annot="${AMPLICON_PIPELINE_SCRIPTSV2}/taxa_annot.R"
